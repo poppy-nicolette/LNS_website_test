2ffe38beeec0c3aac3241047897c013e7b30107b
poppy <75341794+poppy-nicolette@users.noreply.github.com>
2025-03-15 14:44:12 -0300

    Update observablehq.config.js

--- observablehq.config.js
+++ observablehq.config.js
@@ -6,15 +6,15 @@ export default {
   // The pages and sections in the sidebar. If you don’t specify this option,
   // all pages will be listed in alphabetical order. Listing pages explicitly
   // lets you organize them into sections and have unlisted pages.
-  pages: [
-    {
-      name: "Resources",
-      pages: [
-        { name: "Dashboard", path: "/example-dashboard" },
-        { name: "Report", path: "/example-report" },
-      ],
-    },
-  ],
+  //pages: [
+  //  {
+  //    name: "Resources",
+  //    pages: [
+  //      { name: "Dashboard", path: "/example-dashboard" },
+  //       { name: "Report", path: "/example-report" },
+  //     ],
+  //   },
+  // ],
 
   // Content to add to the head of the page, e.g. for a favicon:
   head: '<link rel="icon" href="icon.png" type="image/png" sizes="32x32">',
